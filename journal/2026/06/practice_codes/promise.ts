c3de780026ad42e525e106624e13bde31bcae730
kentosoccer0502 <kentosoccer0502@gmail.com>
2026-06-25 08:19:48 +0900

    📘 2026-06-25 学習記録追加 08:19

--- journal/2026/06/practice_codes/promise.ts
+++ journal/2026/06/practice_codes/promise.ts
@@ -5,6 +5,10 @@ const p = readFile('sample.txt', 'utf-8');
 p.then((data) => {
   console.log('SUCCESS', data);
 });
-p.catch((error) => {
+p.catch((error: unknown) => {
+  // default error type is "any"
   console.log('ERROR', error);
+});
+p.finally(() => {
+  console.log('FINALLY');
 });
