d8c859f4d53de93064485bbc842eb4d98f0a9e22
kentosoccer0502 <kentosoccer0502@gmail.com>
2026-07-25 12:09:49 +0900

    📘 2026-07-25 学習記録追加 12:09

---
+++ leetcode/66. Plus One.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/plus-one/description/?envType=study-plan-v2&envId=programming-skills
+
+function plusOne(digits: number[]): number[] {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i]! !== 9) {
+      digits[i]! += 1;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+}
